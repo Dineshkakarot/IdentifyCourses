58c5ebd361e8cbda564a5df903f56fc2aee0886a
Ambikesh <53272487+Ambikesh-Sarangi@users.noreply.github.com>
2021-05-16 17:35:45 +0530

    Minor Update

--- src/test/java/com/test/testcases/ProductsHomeTest.java
+++ src/test/java/com/test/testcases/ProductsHomeTest.java
@@ -25,6 +25,7 @@ public class ProductsHomeTest extends PageBase {
 	String sheetName2 = "SecondPage";
 	String sheetName3 = "ThirdPage";
 	String sheetName4 = "FourthPage";
+	String sheetName5 = "FifthPage";
 
 	public ProductsHomeTest() {
 
@@ -39,7 +40,7 @@ public class ProductsHomeTest extends PageBase {
 		productsHome = landingPage.navigateToForEnterprise();
 	}
 
-	@Test(priority = 0, groups = "Smoke")
+	//@Test(priority = 0, groups = "Smoke")
 	public void validateFEHomePageTitle() {
 
 		String actualTitle = productsHome.validatePageTitle();
@@ -53,7 +54,7 @@ public class ProductsHomeTest extends PageBase {
 		return data;
 	}
 
-	@Test(priority = 12, dataProvider = "getFirstPageData", groups = "Regression")
+	//@Test(priority = 12, dataProvider = "getFirstPageData", groups = "Regression")
 	public void fillingFormUsingValidDetails(String Fname, String Lname, String JFunc, String JTitle, String Email,
 			String Phone, String Instiname, String Institype, String disp, String country, String state) {
 
@@ -88,7 +89,7 @@ public class ProductsHomeTest extends PageBase {
 		return data;
 	}
 
-	@Test(priority = 1, dataProvider = "getSecondPageData", groups = "Regression")
+	//@Test(priority = 1, dataProvider = "getSecondPageData", groups = "Regression")
 	public void fillingWithoutFirstName(String Lname, String JFunc, String JTitle, String Email, String Phone,
 			String Instiname, String Institype, String disp, String country, String state) {
 
@@ -128,7 +129,7 @@ public class ProductsHomeTest extends PageBase {
 	}
 
 	// Fill the details except lastname and check for error gets displayed or not.
-	@Test(priority = 2, dataProvider = "getThirdPageData", groups = "Regression")
+	//@Test(priority = 2, dataProvider = "getThirdPageData", groups = "Regression")
 	public void fillingWithoutLastName(String Fname, String JFunc, String JTitle, String Email, String Phone,
 			String Instiname, String Institype, String disp, String country, String state) {
 
@@ -166,10 +167,9 @@ public class ProductsHomeTest extends PageBase {
 		return data;
 	}
 
-	// Fill the details except JobFunction and check for error gets displayed or
-	// not.
+	
 
-	@Test(priority = 3, dataProvider = "getFourthPageData", groups = "Regression")
+	//@Test(priority = 3, dataProvider = "getFourthPageData", groups = "Regression")
 	public void fillingWithoutJobFunction(String Fname, String Lname, String JTitle, String Email, String Phone,
 			String Instiname, String Institype, String disp, String country, String state) {
 
@@ -190,6 +190,45 @@ public class ProductsHomeTest extends PageBase {
 
 					Thread.sleep(2000);
 					boolean flag = productsHome.jobFunctionErrorDisplayed();
+					Assert.assertTrue(flag);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				driver.close();
+			}
+			driver.switchTo().window(parentWindow);
+		}
+	}
+	
+	@DataProvider
+	public Object[][] getFifthPageData() {
+
+		Object data[][] = TestUtil.getTestData(sheetName5);
+		return data;
+	}
+
+
+	@Test(priority = 4, dataProvider = "getFifthPageData", groups = "Regression")
+	public void fillingWithoutJobTitle(String Fname, String Lname, String JFunc, String Email, String Phone,
+			String Instiname, String Institype, String disp, String country, String state) {
+
+		parentWindow = driver.getWindowHandle();
+		productsHome.clickonForCampus();
+		Set<String> handles3 = driver.getWindowHandles();
+		Iterator<String> itr3 = handles3.iterator();
+		while (itr3.hasNext()) {
+
+			String childWindow1 = itr3.next();
+			if (!childWindow1.contentEquals(parentWindow)) {
+				driver.switchTo().window(childWindow1);
+				productsHome.clickGetStarted();
+				try {
+					Thread.sleep(1000);
+					productsHome.execelInvalidJobTitle(Fname, Lname, JFunc, Email, Phone, Instiname, Institype, disp,
+							country, state);
+
+					Thread.sleep(2000);
+					boolean flag = productsHome.jobTitleErrorDisplayed();
 					Assert.assertTrue(flag);
 				} catch (Exception e) {
 					e.printStackTrace();
