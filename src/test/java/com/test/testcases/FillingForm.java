90ee3fcfd2db6d56473d88eae1f6156773fdab7a
Ambikesh <53272487+Ambikesh-Sarangi@users.noreply.github.com>
2021-05-18 10:50:41 +0530

    Change test modes

--- src/test/java/com/test/testcases/FillingForm.java
+++ src/test/java/com/test/testcases/FillingForm.java
@@ -4,8 +4,8 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -27,7 +27,7 @@ public class FillingForm extends PageBase {
 		super();
 	}
 
-	@BeforeMethod
+	@BeforeClass
 	public void setUp() {
 
 		setupBrowser();
@@ -110,7 +110,7 @@ public class FillingForm extends PageBase {
 		}
 	}
 
-	@AfterMethod
+	@AfterClass
 	public void tearDown() {
 
 		driver.quit();
